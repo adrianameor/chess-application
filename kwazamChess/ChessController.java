48c575df102bab1f304abce88d7d82b45184a6a8
adrianameor <adrianameorazman@gmail.com>
2025-05-12 14:50:49 +0800

    Update ChessController.java

--- kwazamChess/ChessController.java
+++ kwazamChess/ChessController.java
@@ -1,7 +1,4 @@
-/* NURAFRINA BATRISYIA BINTI NORDZAMAN	1231303327@student.mmu.edu.my
-ADRIANA BINTI MEOR AZMAN	1211111079@student.mmu.edu.my
-NURULAIN AFIQAH BINTI ABDULLAH	1211112326@student.mmu.edu.my
-NUR ADIBAH BINTI KHAIRUL ANUAR	1211112286@student.mmu.edu.my */
+
 
 import java.io.*;
 import java.util.ArrayList;
